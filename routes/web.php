15d062b15f29c11275f3b7b08c5b55259c300342
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-19 14:49:47 +0800

    create route for login/logout

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
-Route::get('/w', function () {
-    return view('login');
-});
+Route::get('/home', function () { return view('pages.home'); })->name('home');
+Route::get('/register', function () { return view('register'); })->name('register');
 
-Route::get('/', function () {
-    return view('pages.home');
-});
-Route::get('/home', function () {
-    return view('pages.home');
-})->name('home');
-
+Route::get('/', function () {return view('login'); })->name('login');
+Route::post('/login', [UserController::class, 'authenticate'])->name('loginAccount');
+Route::resource('users', UserController::class);
 Route::resource('tasks', TaskController::class);
-// Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/logout', function () {
+    Auth::logout();
+    Session::flush();
+    return redirect()->route('login'); 
+})->name('logout');
