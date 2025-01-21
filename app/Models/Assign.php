f149280ab8411ffe620fdb5070e96f53dd15ad88
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-21 14:46:46 +0800

    Define relationships between Assign and Task, User models

--- app/Models/Assign.php
+++ app/Models/Assign.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Assign extends Model
 {
     protected $fillable = ['user_id','task_id'];
+    public function task() {
+        return $this->belongsTo(Task::class);  // Assuming 'Assign' belongs to 'Task'
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);  // Assuming Assign belongs to User
+    }
 }
