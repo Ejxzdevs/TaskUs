83d4179abda5ef0a45f2eeaf40a9acf7317d8db7
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-21 23:08:29 +0800

    modified query

--- app/Services/assignApi.php
+++ app/Services/assignApi.php
@@ -9,7 +9,6 @@ class AssignApi
         return DB::table('tasks')
             ->leftJoin('assigns', 'assigns.task_id', '=', 'tasks.id')
             ->leftJoin('users', 'assigns.user_id', '=', 'users.id')
-            ->where('tasks.task_status', 'Completed')
             ->select('tasks.id as task_id', 'tasks.*', 'assigns.*', 'users.*') 
             ->get();
     }
