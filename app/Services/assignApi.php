be2965effed5e41f4d3330118453e3bf9889b21c
Ejxzdevs <ejhayyygofredo08@gmail.com>
2025-01-21 14:48:37 +0800

    modified statement

--- app/Services/assignApi.php
+++ app/Services/assignApi.php
@@ -1,15 +1,16 @@
 <?php 
 namespace App\Services;
-use App\Models\Assign;
+
+use Illuminate\Support\Facades\DB;
+
 class AssignApi 
 {
-    public static function viewAssignTask($id){
-
-        return $data = Assign::with('task', 'user')
-                    ->whereHas('task', function ($query) {
-                    $query->where('id', 2);  
-        })
-        ->get();
+    public static function viewCompletedTask() {
+        return DB::table('tasks')
+            ->leftJoin('assigns', 'assigns.task_id', '=', 'tasks.id')
+            ->leftJoin('users', 'assigns.user_id', '=', 'users.id')
+            ->where('tasks.task_status', 'Completed')
+            ->select('tasks.id as task_id', 'tasks.*', 'assigns.*', 'users.*') 
+            ->get();
     }
-
 }
